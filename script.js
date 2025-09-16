b505cd3c93cff5bdd428f3b102f7a1b19fd01d71
lateef <117226522+latakin@users.noreply.github.com>
2025-09-16 03:47:14 +0100

    created an onload event for the 16x16 grid

--- script.js
+++ script.js
@@ -2,23 +2,20 @@ const gridSize = document.querySelector("#gridsize");
 const sketchButton = document.querySelector("#enter");
 const container = document.querySelector(".main-container");
 
+document.addEventListener('DOMContentLoaded', ()=> {
+    createDiv(16);
+})
+ 
 
-
-function showSize() {
-    let size = gridSize.value;
-    console.log(size)
-    return size;
-}
 
 sketchButton.addEventListener("click", ()=> {
-    showSize();
-    createDiv()
+    const size = prompt('how many squares do you want? ')
+    container.innerHTML = " ";
+    createDiv(size)
 
 })
     
-function createDiv() {
-    size = showSize()
-    size = Math.min(size, 100)
+function createDiv(size) {
     for(i = 0; i < size; i++) {
         const rowDiv = document.createElement('div');
         rowDiv.setAttribute('class', 'rowdiv')
@@ -27,7 +24,7 @@ function createDiv() {
             const divs = document.createElement('div');
              divs.textContent = " ";
              divs.setAttribute('class', 'coldiv')
-             divs.addEventListener('click', ()=> {
+             divs.addEventListener('mouseover', ()=> {
                 divs.style.backgroundColor = `rgb(
                 ${Math.floor(Math.random() * 255)},
                 ${Math.floor(Math.random() * 255)},
